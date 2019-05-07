6c2f4e8823e8959514b8784e57bb8c2e2d51c4ff
dk1514 <dk1514@nyu.edu>
2019-05-07 17:43:38 -0400

    refactored code

--- src/Pacman/helpers.js
+++
@@ -1,8 +0,0 @@
-import { HEIGHT } from "./constants";
-
-export function cssPosition(position, gridSize) {
-  return {
-    left: (position[0] + 1.5) * gridSize,
-    top: (HEIGHT - position[1] - 3) * gridSize
-  };
-}
---
+++ src/PacmanGame/components/Board/board.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { WALL_COLOUR, WIDTH, HEIGHT } from "../../constants";
+import Line from "../gridline";
+import walls from "./walls";
+import PropTypes from "prop-types";
+import "../../style.scss";
+
+export function Walls(props) {
+  const lineProps = {
+    strokeWidth: 1,
+    stroke: WALL_COLOUR,
+    fill: "none"
+  };
+
+  const linesWalls = Object.keys(walls).map(key => {
+    const parts = walls[key].parts.map(([distance, direction, radius]) => ({
+      distance,
+      direction,
+      radius
+    }));
+
+    return (
+      <Line
+        key={key}
+        {...props}
+        {...lineProps}
+        start={walls[key].start}
+        parts={parts}
+      />
+    );
+  });
+
+  return <g className="walls">{linesWalls}</g>;
+}
+
+export default function Board(props) {
+  const { gridSize } = props;
+
+  const boardWidth = gridSize * WIDTH;
+  const boardHeight = gridSize * HEIGHT;
+
+  return (
+    <div className="pacman-board">
+      <svg width={boardWidth} height={boardHeight}>
+        <rect x={0} y={0} width={boardWidth} height={boardHeight} fill="#000" />
+        <Walls {...props} />
+      </svg>
+    </div>
+  );
+}
+
+Board.propTypes = {
+  gridSize: PropTypes.number.isRequired
+};
